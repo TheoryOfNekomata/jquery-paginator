e5b7a95015c6b3fbe36cfcafb30b1ac59f8e712b
TheoryOfNekomata <allan.crisostomo@outlook.com>
2016-12-16 14:21:39 +0800

    Update gulp file for compiling CSS

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var
      * The path of the source files in order of occurrence.
      * @type {string[]}
      */
-    files = [
+    scripts = [
         './src/init.js',
         './src/settings/component.js',
         './src/settings.js',
@@ -35,32 +35,46 @@ var
 
     gulp = require('gulp'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    csso = require('gulp-csso');
 
 if (name.indexOf('/') > -1) {
     name = name.slice(name.indexOf('/') + 1);
 }
 
-gulp.task('compile', function (cb) {
-    gulp.src(files)
+gulp.task('compile-scripts', function (cb) {
+    gulp.src(scripts)
         .pipe(concat(name + '.js'))
         .pipe(gulp.dest(output))
         .on('end', cb);
 });
 
-gulp.task('compile-minified', function (cb) {
-    gulp.src(files)
+gulp.task('compile-scripts-minified', function (cb) {
+    gulp.src(scripts)
         .pipe(concat(name + '.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(output))
         .on('end', cb);
 });
 
-gulp.task('copy-styles', function (cb) {
+gulp.task('compile-styles', function (cb) {
     gulp.src(styles)
         .pipe(concat(name + '.css'))
         .pipe(gulp.dest(output))
         .on('end', cb);
 });
 
-gulp.task('default', ['compile', 'compile-minified', 'copy-styles']);
+gulp.task('compile-styles-minified', function (cb) {
+    gulp.src(styles)
+        .pipe(concat(name + '.min.css'))
+        .pipe(csso())
+        .pipe(gulp.dest(output))
+        .on('end', cb);
+});
+
+gulp.task('default', [
+    'compile-scripts',
+    'compile-scripts-minified',
+    'compile-styles',
+    'compile-styles-minified'
+]);
